99706790fdb9d8157d5a5dfd42f08b2b83572946
gaurav <gauravkumar5823@gmail.com>
2019-09-22 14:56:38 +1200

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,37 @@
+ <!-- this is welcome page   -->
+<?php
+ require 'server.php';
+ ?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Assignment 1</h1>
+
+				<a href="logout.php">Logout</a>     <!-- move to logout page  -->
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Home Page</h2>
+
+			
+      <?php if(isset($_SESSION['First_Name'])): ?>
+   <p> welcome back <strong><?php echo'  '. $_SESSION['First_Name']; ?></strong></p>         <!-- show the logged in user name   -->
+
+ <?php endif ?>
+
+
+
+
+		</div>
+	</body>
+</html>
