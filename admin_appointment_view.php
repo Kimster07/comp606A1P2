dedcc350c68aca0531da2a2582821d291ec10a92
Kimster07 <52937251+Kimster07@users.noreply.github.com>
2019-09-23 22:10:53 +1200

    Create admin_appointment_view.php

---
+++ admin_appointment_view.php
@@ -0,0 +1,130 @@
+ 
+<?php
+session_start();
+if(!isset($_SESSION['aid']))
+{
+	?>
+	<script type="text/javascript">
+		window.open('adminlogin.php','_self');
+	</script>
+	<?php
+}
+?>
+<?php include('function.php'); ?>
+
+<?php
+$apid = $_GET['apid'];
+$data = getApDetail($apid)
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Appointments Detail</title>
+	
+	<!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- css -->
+    <style>
+      body{
+
+      background: url(Images/Massage2.jpg) no-repeat; 
+      background-size: cover;
+      background-attachment: fixed;
+
+      }
+
+
+    
+
+    </style>
+
+</head>
+
+
+
+<body>
+
+	<div class="container">
+
+
+	<!-- navbar -->
+	<nav class="navbar navbar-default">
+	<div class="container-fluid">
+
+		<!-- Brand and toggle get grouped for better display -->
+		<div class="navbar-header">
+
+		<!-- Button that toggles the navbar on and off on small screens -->
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	
+			<!-- Hides information from screen readers -->
+			<span class="sr-only"></span>
+		
+			<span class="icon-bar"></span>
+
+		</button>
+
+		<a class="pull-left"><img src="Images/YY.svg" width="60px" height="60px"></a>
+		<a class="navbar-brand" href="#">Hamilton Massage Center</a>
+
+		</div>
+
+		<!-- nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		<div class="navbar-form navbar-right">
+			<ul class="nav navbar-nav">
+				<li><a href="admin_logout.php">Logout</a></li>
+				<li><a href="admin_dashboard.php">Back</a></li>
+
+			</ul>
+		
+		</div>
+		</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+	</nav>
+
+
+	</div>
+
+
+
+
+
+	<div class="page-header">
+	<h1 align="center">Appointment Details</h1>
+	</div>
+
+	<!-- appointment details -->
+	<table class="table-condensed">
+		<tr>
+			<th>Name: </th>
+			<td><?php echo $data[0]." ".$data[5]; ?></td>
+		</tr>
+		<tr>
+			<th>Email: </th>
+			<td><?php echo $data[6]; ?></td>
+		</tr>
+		<tr>
+			<th>Date: </th>
+			<td><?php echo $data[1]; ?></td>
+		</tr>
+		<tr>
+			<th>Time Slot: </th>
+			<td><?php echo $data[2]; ?></td>
+		</tr>
+		<tr>
+			<th>Type: </th>
+			<td><?php echo $data[3]; ?></td>
+		</tr>
+		<tr>
+			<th>Comment: </th>
+			<td><?php echo $data[4]; ?></td>
+		</tr>
+
+	</table>
+
+</body>
