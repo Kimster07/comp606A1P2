3eb23385e22f1fa6775c10b21e103ea92512087d
Kimster07 <52937251+Kimster07@users.noreply.github.com>
2019-09-23 22:12:16 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,125 @@
+<head>
+    
+	<meta charset="UTF-8">
+
+	<!-- If IE use the latest rendering engine -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!-- Set the page to the width of the device and set the zoon level -->
+	<meta name="viewport" content="width = device-width, initial-scale = 1">
+	<title>Sports Massage</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<style>
+
+		body{
+			background-image: url(Images/Massage1.jpg);
+			background-size: cover;
+			background-attachment: fixed;
+		}
+	
+		
+	</style>
+
+</head>
+
+<body>
+
+	<div class="container">
+
+
+        <!-- navbar -->
+        <nav class="navbar navbar-default">
+          	<div class="container-fluid">
+      
+				<!-- Brand and toggle get grouped for better display -->
+				<div class="navbar-header">
+		
+					<!-- Button that toggles the navbar on and off on small screens -->
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			
+						<!-- Hides information from screen readers -->
+						<span class="sr-only"></span>
+				
+						<!-- Draws bars in navbar button when in small mode -->
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+
+					</button>
+		
+					<a class="pull-left"><img src="Images/YY.svg" width="60px" height="60px"></a>
+					<a class="navbar-brand" href="#">Hamilton Massage Center</a>
+
+				</div>
+
+					<!-- nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<div class="navbar-form navbar-right">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+							<li><a href="login.php">Log In</a></li>
+							<li><a href="Signup.php">Sign Up</a></li>
+							<li><a href="adminlogin.php" >Admin</a></li>
+						</ul>
+				
+					</div>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+
+
+	</div>
+
+	<!-- Start of one row and four coloums -->
+
+	<br><br><br><br><br><br><br>
+
+	<!-- Thumbnails -->
+	<div class="container">
+	<div class="row">
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div class="thumbnail" style="background-color: rgba(255,255,255,0.5);">
+				<div class="caption">
+					<h4>Sports Massage</h4>
+					<p>Sports massage is a form of bodywork geared toward participants in athletics. It is used to help prevent injuries, to prepare the body for athletic activity and maintain it in optimal condition, and to help athletes recover from workouts and injuries.</p><br>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div class="thumbnail" style="background-color: rgba(255,255,255,0.5);">
+				<div class="caption">
+					<h4>Deep Tissue Massage</h4>
+					<p>Deep tissue massage is a type of massage therapy that focuses on realigning deeper layers of muscles and connective tissue. It is especially helpful for chronic aches and pains and contracted areas such as stiff neck and upper back, low back pain, leg muscle tightness, and sore shoulders.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div class="thumbnail" style="background-color: rgba(255,255,255,0.5);">
+				<div class="caption">
+					<h4>Therapeutic Massage</h4>
+					<p>Therapeutic massage incorporates a variety of advanced modalities that enhance the body's natural restorative functioning. Light to firm touch is used to release tension, relax muscles, increase blood and lymph circulation, and impart a sense of calm.</p><br>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+			<div class="thumbnail" style="background-color: rgba(255,255,255,0.5);">
+				<div class="caption">
+					<h4>Relaxation Massage</h4>
+					<p>Relaxation massage is a smooth, gentle, flowing style that promotes general relaxation, relieves muscular tension, plus improves circulation and range of movement.</p><br><br><br><br>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	</div>
+
+
+</body>
+
+
+ 
